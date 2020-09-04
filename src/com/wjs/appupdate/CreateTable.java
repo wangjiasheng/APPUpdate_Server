6f37837a379f8d20ff32dc7729d907a1496d7742
wangjiasheng <314232332@qq.com>
2020-09-04 10:27:24 +0800

    我的代码

---
+++ src/com/wjs/appupdate/CreateTable.java
@@ -0,0 +1,33 @@
+package com.wjs.appupdate;
+
+import com.wjs.temp.DBUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CreateTable  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/json; charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        try {
+            DBUtils.main(new String[]{});
+            writer.println("sucess");
+        } catch (Exception e) {
+            writer.println("no");
+            e.printStackTrace();
+        }
+        writer.close();
+    }
+}
